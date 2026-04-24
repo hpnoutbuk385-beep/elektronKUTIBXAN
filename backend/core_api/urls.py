bef0b485318ad9aa0563644930bf495de2d4110a
ibragimov_wx <hpnoutbuk385@gmail.com>
2026-04-24 18:27:06 +0500

    Fixed ImportError: removed RegisterView from urls.py

--- backend/core_api/urls.py
+++ backend/core_api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     OrganizationViewSet, BookViewSet, TransactionViewSet, LeaderboardViewSet,
     QuizViewSet, CompetitionViewSet, RewardViewSet, PurchaseViewSet,
-    RegisterView, ProfileView, NameLoginView, SchoolClassViewSet, UserViewSet,
+    ProfileView, NameLoginView, SchoolClassViewSet, UserViewSet,
     NewsViewSet
 )
 from .reports import SchoolReportPDFView
